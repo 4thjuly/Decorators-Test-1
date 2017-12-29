63016a3a6da7b1cba9a1678be8f2d9cd877919f2
Ian Ellison-Taylor <4thjuly@ellison-taylor.com>
2017-12-28 21:16:07 -0800

    Hook setState

--- src/Widget.tsx
+++ src/Widget.tsx
@@ -1,15 +1,7 @@
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, Button } from 'react-native';
 
-interface Props {
-  prop1: string
-}
-
-interface State {
-  state1: string 
-}
-
-function guidClassDecorator(guid: string) {
+function MrCloud(guid: string, sharedState: string[]) {
 
   // Generate instances of a class
   function construct(constructor, args) {
@@ -18,37 +10,50 @@ function guidClassDecorator(guid: string) {
     return new c();
   }
 
-  function classDecorator(target: any, guid: string) {
-    var original = target;
+  function classDecorator(constructor: any, guid: string, sharedState: string[]) {
+    var originalConstructor = constructor;
 
     // New constructor function
     var f: any = function (...args) {
-      console.log("[Decorator] New: " + original.name); 
-      var newObj  = construct(original, args);
+      console.log("[Decorator] New: " + originalConstructor.name); 
+      var newObj = construct(originalConstructor, args);
       newObj.__classDecorator_guid = guid;
+      // Hook setState
+      var originalSetState = newObj['setState'];
+      newObj['setState'] = function(...args) {
+        console.log("[Decorator] SetState: ", args[0]);
+        originalSetState.apply(newObj, args);
+      }
       return newObj;
     }
-  
+
     // copy prototype so intanceof operator still works
-    f.prototype = original.prototype;
+    f.prototype = originalConstructor.prototype;
   
     // return new constructor (will override original)
     return f;
   }
 
-  return (target) => classDecorator(target, guid);
-
+  return (target) => classDecorator(target, guid, sharedState);
 }
 
-@guidClassDecorator('1234-5678')
+interface Props {
+  prop1: string
+}
+
+interface State {
+  state1: number 
+}
+
+@MrCloud('1234-5678', ['state1'] )
 export class Widget extends React.Component<Props, State> {
   constructor(props) {
     super(props);
-    this.state = { state1: 'constructor'};
+    this.state = { state1: 0 };
   }
 
   componentDidMount() { 
-    this.setState( { state1: 'didMount' });
+    this.setState( { state1: 1 });
   }
 
   render() {
@@ -58,9 +63,15 @@ export class Widget extends React.Component<Props, State> {
         <Text style={styles.text}> A Widget </Text>
         <Text style={styles.text}> Prop1: { this.props.prop1 } </Text>
         <Text style={styles.text}> State: { this.state.state1 } </Text>
+        <Button onPress={ () => this.updateState() } title='  Update State  ' />
       </View>
     );
   }
+
+  updateState() {
+    this.setState({state1: this.state.state1 + 1});
+  }
+
 }
 
 const styles = StyleSheet.create({
